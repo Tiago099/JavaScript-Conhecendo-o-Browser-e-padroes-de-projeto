739b8d77ecbc9e74fb9913d8cb71c0706f8d1751
Tiago Sebastian <tiagosebastian99@gmail.com>
2022-03-03 23:05:51 -0300

    finalizando o curso de javScript

--- aluraframe/client/js/app/views/NegociacoesView.js
+++ aluraframe/client/js/app/views/NegociacoesView.js
@@ -1,9 +1,10 @@
-class NegociacoesView {
+class NegociacoesView extends View {
 
     constructor(elemento){
-        this._elemento = elemento;
+        super(elemento);
     }
-        _template(model){
+
+        template(model){
 
             return `
             <table class="table table-hover table-bordered">
@@ -26,7 +27,7 @@ class NegociacoesView {
                            <td>${n.volume}</td>
                         </t>
                     `
-                    ).join('')}
+                      ).join('')}
                 </tbody>
                 
                 <tfoot>
@@ -38,11 +39,6 @@ class NegociacoesView {
 
             </table>
             `;
-        }
-        update(model) {
-
-            this._elemento.innerHTML = this._template(model);
-
         }
     
 }
